5c3cc926bf126e786edb724a77017fd1c12f3404
iceG <icymail194@gmail.com>
2023-11-01 22:17:39 +0200

    ready

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -4,6 +4,9 @@
 
 using namespace std;
 
+// TASK 1 ----------------------------------------------------------------------
+
+/*
 class Rectangle {
 
 	private:
@@ -11,18 +14,37 @@ class Rectangle {
 		int width;
 
 	public:
-		void getHeight(int h) {
-			int height = h;
+		void setHeight() {
+			int h;
+			cout << "Enter the height of rectangle: ";
+			cin >> h;
+			height = h;
 		}
-		void getWidth(int w) {
-			int width = w;
+
+		int getHeight() {
+			return this->height;
 		}
+
+		void setWidth() {
+			int w;
+			cout << "Enter the width of rectangle: ";
+			cin >> w;
+			width = w;
+		}
+
+		int getWidth() {
+			return this->width;
+		}
+
 		void sqr() {
-			cout << "The square is: " << height * width;
+			cout << endl << "The square is: " << height * width;
 		}
-		int p(int h, int w) {
-			cout << "The perimetre is: ";
-				return ((h + w) * 2);
+		void perimetre() {
+			cout << endl<< "The perimetre is: "<<
+				 ((height + width) * 2);
+		}
+		void draw() {
+			cout <<endl<< " ----" << endl << "|    |" <<endl<< " ----";
 		}
 	};
 
@@ -30,10 +52,54 @@ class Rectangle {
 int main() 
 {
 	Rectangle r;
-	r.getHeight(3);
-	r.getWidth(2);
+	r.setHeight();
+	r.setWidth();
 	r.sqr();
-	
+	r.perimetre();
+	r.draw();
+
+	return 0;
+}
+
+*/
+
+// TASK 2 ----------------------------------------------------------------------
+
+
+	class Triangle {
+	private:
+		int catet_1;
+		int catet_2;
+
+	public:
+		void setCatets() {
+			int c1, c2;
+			cout << "Enter the length of the first catet: ";
+			cin >> c1;
+			catet_1 = c1;
+			cout << "Enter the length of the second catet: ";
+			cin >> c2;
+			catet_2 = c2;
+		}
+
+		void square() {
+			cout << "The square of triangle is: " << ((catet_1 * catet_2) / 2)<< endl;
+		}
+
+		void gipotenusa() {
+			int g = (catet_1*catet_1) + (catet_2*catet_2);
+			cout << "Gipotenusa is: "<< sqrt(g);
+		}
+
+	};
+
+int main() {
+
+	Triangle t;
+	t.setCatets();
+	t.square();
+	t.gipotenusa();
+
 
 	return 0;
 }
