1928f7dd247cc12a94cc15a3ddfbab04bfebaded
iceG <icymail194@gmail.com>
2023-11-10 18:42:07 +0200

    1

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,21 +1,60 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <vector>
 
 using namespace std;
 
+class Basic {
+	public:
+		virtual void square() = 0;
+	};
+
+class Rect : public Basic {
+public:
+	void square(){
+	cout << "Square of Rect" << endl;
+	}
+};
+
+class Circuit : public Basic {
+public:
+	void square() {
+		cout << "Square of Circuit" << endl;
+	}
+};
+
+class Triangle : public Basic {
+public:
+	void square() {
+		cout << "Square of Triangle" << endl;
+	}
+};
+
+class Trapeze : public Basic {
+public:
+	void square() {
+		cout << "Square of Trapeze" << endl;
+	}
+};
+
+
 int main() 
 {
+	Basic* rect = new Rect();
+	Basic* circ = new Circuit();
+	Basic* tri = new Triangle();
+	Basic* trap = new Trapeze();
 
-	cout << "Enter the time in seconds from start of the day: ";
-	int seconds;
-	cin >> seconds;
+	vector <Basic*> base;
+	base.push_back(rect);
+	base.push_back(circ);
+	base.push_back(tri);
+	base.push_back(trap);
 
-	int hours = (seconds/60)/60;
-	int minutes = (seconds - hours*60*60)/60;
-	int secondsNow = seconds - (seconds - hours * 60 * 60) - minutes*60;
-
-	cout << endl << "Time now is: " << hours << " hours " << minutes << " minutes " << seconds << " seconds ";
+	for (auto el : base) {
+		el->square();
+	}
 
 
 	return 0;
