e8f936b8684bbb5b4526c69ab68b31c537618475
iceG <icymail194@gmail.com>
2023-11-10 12:27:47 +0200

    reaady

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -5,46 +5,98 @@
 using namespace std;
 
 // TASK 1 ----------------------------------------------------------------------
+//
+//
+//class Vehicle {
+//public:
+//	
+//	virtual void start() {
+//		cout << "Started!"<<endl;
+//	};
+//
+//	virtual void stop() {
+//		cout << "Stopped!"<<endl;
+//	};
+//
+//};
+//
+//class Car : public virtual Vehicle {
+//public:
+//	void start() {
+//		cout << "Started in Car!"<<endl;
+//	}
+//	void stop() {
+//		cout << "Stopped in Car!"<<endl;
+//	};
+//};
+//
+//class Motorcycle : public virtual Vehicle {
+//public:
+//	void acrobatic() {
+//		cout << "Acrobatic!"<<endl;
+//	}
+//};
+//
+//
+//int main() {
+//
+//	Vehicle mercedes;
+//	Motorcycle moto;
+//	Car redMerc;
+//	mercedes.start();
+//	mercedes.stop();
+//	redMerc.start();
+//	redMerc.stop();
+//	moto.acrobatic();
+//
+//
+//	return 0;
+//}
 
 
-class Vehicle {
+// TASK 2 ----------------------------------------------------------------------
+
+class MediaPlayer {
 public:
-	
 	virtual void start() {
-		cout << "Started!";
-	};
-
-	virtual void stop() {
-		cout << "Stopped!";
-	};
-
-};
-
-class Car : public Vehicle {
-	virtual void start() {
-		cout << "Started in Car!";
+		cout << "Start" << endl;
 	}
 	virtual void stop() {
-		cout << "Stopped in Car!";
-	};
-};
-
-class Motorcycle : public Vehicle {
-
-	void acrobatic() {
-		cout << "Acrobatic!";
+		cout << "Stop" << endl;
+	}
+	virtual void pause() {
+		cout << "Pause" << endl;
 	}
 };
 
+class AudioPlayer: public MediaPlayer {
+public:
+	void loud() {
+		cout << "Make it loud" << endl;
+	}
+};
+
+class VideoPlayer : public MediaPlayer {
+public:
+	void rewind() {
+		cout << "Rewind record" << endl;
+	}
+};
 
 int main() {
+	
 
-	Car mercedes;
-	Motorcycle moto;
-	mercedes.start();
-	mercedes.stop();
-	moto.acrobatic();
+	MediaPlayer m;
+	AudioPlayer a;
+	VideoPlayer v;
 
+	m.start();
+	m.pause();
+	m.stop();
+	a.start();
+	a.loud();
+	v.rewind();
+	v.stop();
 
 	return 0;
 }
