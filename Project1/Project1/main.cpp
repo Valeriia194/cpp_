4f5b391f88ef05a8227e1cdba3b8e76877dcb0ef
Valeriia_Nikitenko <icymail194@gmail.com>
2024-01-04 14:36:50 +0200

    ready

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,22 +1,107 @@
-#include <iostream>
-#include <string>
-#include <cmath>
 
+// Task with sortation
+//__________________________________________________________________________________
+
+
+
+//#include <iostream>
+//#include <string>
+//#include <cmath>
+//#include <vector>
+//
+//using namespace std;
+//
+//void selectionSort(vector<int>& arr) {
+//    int n = arr.size();
+//    for (int i = 0; i < n - 1; i++) {
+//        int minIndex = i;
+//        for (int j = i + 1; j < n; j++)
+//            if (arr[j] < arr[minIndex])
+//                minIndex = j;
+//        if (minIndex != i)
+//            swap(arr[i], arr[minIndex]);
+//    }
+//}
+//
+//void insertionSort(vector<int>& arr) {
+//    int n = arr.size();
+//    for (int i = 1; i < n; i++) {
+//        int key = arr[i];
+//        int j = i - 1;
+//        while (j >= 0 && arr[j] > key) {
+//            arr[j + 1] = arr[j];
+//            j = j - 1;
+//        }
+//        arr[j + 1] = key;
+//    }
+//}
+//
+//
+//int main() {
+//    vector<int> arr = { 30, 40, 20, 50, 10 };
+//    selectionSort(arr);
+//    cout << " Sorted selection array: \n";
+//    for (int i = 0; i < arr.size(); i++)
+//        cout << arr[i] << " ";
+//    insertionSort(arr);
+//    cout << "\n Sorted insertion array: \n";
+//    for (int i = 0; i < arr.size(); i++)
+//        cout << arr[i] << " ";
+//    return 0;
+//}
+//
+
+
+// Task with searching
+//__________________________________________________________________________________
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int main() 
-{
 
-	cout << "Enter the time in seconds from start of the day: ";
-	int seconds;
-	cin >> seconds;
-
-	int hours = (seconds/60)/60;
-	int minutes = (seconds - hours*60*60)/60;
-	int secondsNow = seconds - (seconds - hours * 60 * 60) - minutes*60;
-
-	cout << endl << "Time now is: " << hours << " hours " << minutes << " minutes " << seconds << " seconds ";
+int linearSearch(vector<int>& arr, int target) {
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] == target)
+            return i; 
+    }
+    return -1; 
+}
 
 
-	return 0;
+int binarySearch(vector<int>& arr, int target) {
+    int left = 0;
+    int right = arr.size() - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target)
+            return mid; 
+        if (arr[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return -1; 
+}
+
+int main() {
+    vector<int> arr = { 10, 20, 30, 40, 50 };
+    int target = 30;
+
+    int result = linearSearch(arr, target);
+    if (result != -1)
+        cout << "Position by linear search: " << result << "\n";
+    else
+        cout << "Element by linear search not found! \n";
+
+
+    result = binarySearch(arr, target);
+    if (result != -1)
+        cout << "Position by binary search: " << result << "\n";
+    else
+        cout << "Element by binary search not found! \n";
+
+    return 0;
 }
