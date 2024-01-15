d17c106af2f31be69391d9608f9571c7a76b53b0
Valeriia_Nikitenko <icymail194@gmail.com>
2024-01-15 23:18:44 +0200

    ready

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,22 +1,82 @@
+// TASK 1 
+//_________________________________________________________________________________
+//
+//#include <iostream>
+//#include <string>
+//
+//int main() {
+//    std::string str;
+//    std::cout << "Enter the string with brackets: ";
+//    std::getline(std::cin, str);
+//
+//    int open_bracket = str.find('(');
+//    int close_bracket = str.find(')');
+//
+//    if (open_bracket == std::string::npos || close_bracket == std::string::npos) {
+//        std::cout << "Absent one bracket.\n";
+//    }
+//    else {
+//        str.erase(open_bracket + 1, close_bracket - open_bracket - 1);
+//        std::cout << "New string: " << str << '\n';
+//    }
+//
+//    return 0;
+//}
+
+
+// TASK 2 
+//_________________________________________________________________________________
+//#include <iostream>
+//#include <string>
+//
+//int main() {
+//    std::string s;
+//    std::cout << "Enter the word: ";
+//    std::cin >> s;
+//
+//    int count[26][26] = { 0 };
+//    for (int i = 0; i < s.size() - 1; ++i) {
+//        count[s[i] - 'a'][s[i + 1] - 'a']++;
+//    }
+//
+//    int different_pairs = 0;
+//    for (int i = 0; i < 26; ++i) {
+//        for (int j = 0; j < 26; ++j) {
+//            if (count[i][j] > 0) {
+//                different_pairs++;
+//                std::cout << char(i + 'a') << char(j + 'a') << " has " << count[i][j] << " pairs\n";
+//            }
+//        }
+//    }
+//
+//    std::cout << "In word " << different_pairs << " different pairs.\n";
+//
+//    return 0;
+//}
+
+// TASK 3
+//_________________________________________________________________________________
+
 #include <iostream>
 #include <string>
-#include <cmath>
 
-using namespace std;
+int main() {
+    std::string sentence;
+    std::cout << "Enter the sentence: ";
+    std::getline(std::cin, sentence);
 
-int main() 
-{
+    int pos = 0;
+    while ((pos = sentence.find(' ')) != std::string::npos) {
+        std::string word = sentence.substr(0, pos);
+        if (word.size() >= 3) {
+            std::cout << word << ' ';
+        }
+        sentence.erase(0, pos + 1);
+    }
 
-	cout << "Enter the time in seconds from start of the day: ";
-	int seconds;
-	cin >> seconds;
+    if (sentence.size() >= 3) {
+        std::cout << sentence;
+    }
 
-	int hours = (seconds/60)/60;
-	int minutes = (seconds - hours*60*60)/60;
-	int secondsNow = seconds - (seconds - hours * 60 * 60) - minutes*60;
-
-	cout << endl << "Time now is: " << hours << " hours " << minutes << " minutes " << seconds << " seconds ";
-
-
-	return 0;
+    return 0;
 }
