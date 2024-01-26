dddf557ac0dddc7d0f3e945b0b6896b598222690
Valeriia_Nikitenko <icymail194@gmail.com>
2024-01-26 23:22:35 +0200

    Basic ready

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,22 +1,61 @@
 #include <iostream>
 #include <string>
-#include <cmath>
+
 
 using namespace std;
 
+
+
 int main() 
 {
+	string playerName;
+	int playerHealth;
+	string playerClass;
 
-	cout << "Enter the time in seconds from start of the day: ";
-	int seconds;
-	cin >> seconds;
+	//Enter Data___________________________________
 
-	int hours = (seconds/60)/60;
-	int minutes = (seconds - hours*60*60)/60;
-	int secondsNow = seconds - (seconds - hours * 60 * 60) - minutes*60;
+	cout << "Enter the Player name: " << endl;
+	cin >> playerName;
 
-	cout << endl << "Time now is: " << hours << " hours " << minutes << " minutes " << seconds << " seconds ";
+	cout << "Enter the Player health: " << endl;
+	cin >> playerHealth;
 
+	cout << "Enter the Player Class(Mage or Warrior): " << endl;
+	cin >> playerClass;
+
+	while (playerHealth > 0) {
+		cout << "Enter the damage for " << playerName << ":" << endl;
+		int damage;
+		cin >> damage;
+
+	// Checked the Class________________________________
+
+		if (playerClass == "Mage") {
+			if (damage%2==0) {
+				playerHealth = playerHealth - damage*2;
+			}
+			else {
+				playerHealth = playerHealth - damage;
+			}
+		}
+		else if (playerClass == "Warrior") {
+			if (damage % 2 == 0) {
+				cout << "No damage!" << endl;
+			}
+			else {
+				playerHealth = playerHealth - damage * 3;
+			}
+		}
+		else {
+			cout << "Wrong Class!" << endl;
+		}
+
+	// Checked is Dead_______________________________
+
+		if (playerHealth <= 0) {
+			cout << playerName << " is DEAD!";
+		}
+	}
 
 	return 0;
 }
