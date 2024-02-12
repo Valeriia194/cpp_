424712632e11ed0f578b5f7edf7e171b513f734a
Valeriia_Nikitenko <icymail194@gmail.com>
2024-02-12 13:57:19 +0200

    ready

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -4,19 +4,51 @@
 
 using namespace std;
 
+class Building {
+
+private:
+	int id;
+	int maxAge;
+	int cost;
+
+	int buildAge = 0;
+	int currentCost;
+
+	void destroy() {
+		cout << "Build with id " << id << " was destroying with age " << buildAge << endl;
+	}
+
+public:
+
+	Building(int _id, int _maxAge, int _cost) : id(_id), maxAge(_maxAge), cost(_cost) {
+		currentCost = cost;
+	}
+
+	int getCost() {
+		return currentCost;
+	}
+
+	void toAge(int years) {
+		buildAge += years;
+		if (maxAge<buildAge) {
+			destroy();
+		}
+		else {
+			currentCost = cost - (cost / maxAge) * buildAge;
+		}
+	}
+
+};
+
 int main() 
 {
+	Building buildOne(1, 10, 1000);
+	cout << "Cost of this building is: " << buildOne.getCost() << "\n\n";
 
-	cout << "Enter the time in seconds from start of the day: ";
-	int seconds;
-	cin >> seconds;
+	buildOne.toAge(5);
+	cout << "5 years ago the cost of build is: " << buildOne.getCost() << endl;
 
-	int hours = (seconds/60)/60;
-	int minutes = (seconds - hours*60*60)/60;
-	int secondsNow = seconds - (seconds - hours * 60 * 60) - minutes*60;
-
-	cout << endl << "Time now is: " << hours << " hours " << minutes << " minutes " << seconds << " seconds ";
-
+	buildOne.toAge(7);
 
 	return 0;
 }
