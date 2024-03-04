9182358dc7e5d8d1b53fd82af676df8f5ed4cca9
Valeriia_Nikitenko <icymail194@gmail.com>
2024-03-04 17:54:50 +0200

    ready

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,22 +1,64 @@
 #include <iostream>
-#include <string>
-#include <cmath>
-
-using namespace std;
-
-int main() 
-{
-
-	cout << "Enter the time in seconds from start of the day: ";
-	int seconds;
-	cin >> seconds;
-
-	int hours = (seconds/60)/60;
-	int minutes = (seconds - hours*60*60)/60;
-	int secondsNow = seconds - (seconds - hours * 60 * 60) - minutes*60;
-
-	cout << endl << "Time now is: " << hours << " hours " << minutes << " minutes " << seconds << " seconds ";
+#include <list> 
 
 
-	return 0;
+template <typename T>
+class List {
+public:
+    virtual void push(const T& val) = 0;
+    virtual T pop() = 0;
+    virtual bool empty() const = 0;
+    virtual ~List() {}
+};
+
+
+template <typename T>
+class Stack : public List<T> {
+private:
+    std::list<T> data; 
+public:
+    void push(const T& val) override {
+        data.push_back(val);
+    }
+    T pop() override {
+        T val = data.back();
+        data.pop_back();
+        return val;
+    }
+    bool empty() const override {
+        return data.empty();
+    }
+};
+
+
+template <typename T>
+class Queue : public List<T> {
+private:
+    std::list<T> data; 
+public:
+    void push(const T& val) override {
+        data.push_back(val);
+    }
+    T pop() override {
+        T val = data.front();
+        data.pop_front();
+        return val;
+    }
+    bool empty() const override {
+        return data.empty();
+    }
+};
+
+int main() {
+    Stack<int> myStack;
+    myStack.push(10);
+    myStack.push(20);
+    std::cout << "Popped from stack: " << myStack.pop() << std::endl;
+
+    Queue<std::string> myQueue;
+    myQueue.push("Hello");
+    myQueue.push("World");
+    std::cout << "Dequeued from queue: " << myQueue.pop() << std::endl;
+
+    return 0;
 }
