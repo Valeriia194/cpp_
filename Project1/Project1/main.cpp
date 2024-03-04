5442f3a965df3e3a6dda25c961981167bd546bb2
Valeriia_Nikitenko <icymail194@gmail.com>
2024-03-04 13:55:57 +0200

    ready

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,22 +1,180 @@
-#include <iostream>
-#include <string>
-#include <cmath>
+//LW 1
+//____________________________________________________________________________________
 
-using namespace std;
+//#include <iostream>
+//#include <string>
+//#include <cmath>
+//#include <vector>
+//
+//using namespace std;
+//
+//class Passport {
+//protected:
+//	string name;
+//	string surname;
+//	int id;
+//public:
+//	Passport(string _name, string _surname, int _id) : name(_name), surname(_surname), id(_id) {
+//
+//	}
+//};
+//
+//class ForeignPassport : public Passport {
+//private:
+//	int foreignId;
+//	vector <string> visas;
+//public:
+//	ForeignPassport (string _name, string _surname, int _id, int _foreignId) : Passport(name, surname, id), foreignId(_foreignId)
+//	{
+//	}
+//	void addVisa(string visa) {
+//		visas.push_back(visa);
+//	}
+//};
+//
+//int main() 
+//{
+//
+//
+//	return 0;
+//}
 
-int main() 
-{
 
-	cout << "Enter the time in seconds from start of the day: ";
-	int seconds;
-	cin >> seconds;
+//LW 2
+//____________________________________________________________________________________
 
-	int hours = (seconds/60)/60;
-	int minutes = (seconds - hours*60*60)/60;
-	int secondsNow = seconds - (seconds - hours * 60 * 60) - minutes*60;
+//#include <iostream>
+//#include <string>
+//#include <cmath>
+//#include <vector>
+//
+//using namespace std;
 
-	cout << endl << "Time now is: " << hours << " hours " << minutes << " minutes " << seconds << " seconds ";
+//class Transport {
+//public:
+//    virtual double calculateTime(double distance) = 0; 
+//    virtual double calculateCost(double weight) = 0; 
+//};
+//
+//class Car : public Transport {
+//public:
+//    double calculateTime(double distance) override {
+//        double speed = 60.0; 
+//        return distance / speed;
+//    }
+//    double calculateCost(double weight) override {
+//        double costPerKg = 2.0; 
+//        return weight * costPerKg;
+//    }
+//};
+//
+//
+//class Bicycle : public Transport {
+//public:
+//    double calculateTime(double distance) override {
+//        double speed = 15.0; 
+//        return distance / speed;
+//    }
+//    double calculateCost(double weight) override {
+//        return 0.0; 
+//    }
+//};
+//
+//class Wagon : public Transport {
+//public:
+//    double calculateTime(double distance) override {
+//        double speed = 5.0; 
+//        return distance / speed;
+//    }
+//    double calculateCost(double weight) override {
+//        double costPerKg = 1.0;
+//        return weight * costPerKg;
+//    }
+//};
+//
+//int main() {
+//
+//    return 0;
+//}
+
+//HW 1
+//____________________________________________________________________________________
+
+//#include <iostream>
+//#include <string>
+//#include <cmath>
+//#include <vector>
+//
+//using namespace std;
+
+//class Student {
+//protected:
+//    string name;
+//    string surname;
+//    string university;
+//public:
+//    Student(string name, string surname, string university)
+//        : name(name), surname(surname), university(university) {}
+//
+//};
+//
+//
+//class Aspirant : public Student {
+//private:
+//    string thesisTopic; 
+//public:
+//    Aspirant(string name, string surname, string university, string thesis)
+//        : Student(name, surname, university), thesisTopic(thesis) {}
+//
+//};
 
 
-	return 0;
-}
+//HW 2
+//____________________________________________________________________________________
+
+//#include <iostream>
+//#include <string>
+//#include <cmath>
+//#include <vector>
+//
+//using namespace std;
+
+
+//class Pet {
+//protected:
+//    string name;
+//    string characteristics;
+//public:
+//    Pet(string name, string characteristics)
+//        : name(name), characteristics(characteristics) {}
+//
+//};
+//
+//
+//class Dog : public Pet {
+//public:
+//    Dog(string name, string characteristics)
+//        : Pet(name, characteristics) {}
+//
+//};
+//
+//
+//class Cat : public Pet {
+//public:
+//    Cat(std::string name, std::string characteristics)
+//        : Pet(name, characteristics) {}
+//
+//};
+//
+//
+//class Parrot : public Pet {
+//public:
+//    Parrot(string name, string characteristics)
+//        : Pet(name, characteristics) {}
+//
+//};
+//
+//int main() {
+//
+//    return 0;
+//}
