493fb4ac3e5709d106229a3a23a31b7170cf9185
Valeriia_Nikitenko <icymail194@gmail.com>
2024-03-13 15:25:09 +0200

    1

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,22 +1,49 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
+// Words
+vector <string> words{
+	"apple", "coffee", "monkey",
+	"table", "language", "city",
+	"player", "kitchen", "vinyl"
+};
+
+//Random word
+string getRandom() {
+	int sizeVocabulary = words.size();
+	int randomWord = rand() % sizeVocabulary;
+	return words[randomWord];
+}
+
+// Display Hangman
+void displayHang(int mistakes) {
+	const int maxMistakes = 5;
+	string hangman[] = {
+		"  +---+",
+		"  |   |",
+		"  O   |",
+		" /|\\  |",
+		" / \\  |",
+		"      |",
+		"========="
+	};
+	for (int i = 0; i < maxMistakes - mistakes; ++i) {
+		std::cout << hangman[i] << std::endl;
+	}
+}
+
 int main() 
 {
+	srand(time(NULL));
 
-	cout << "Enter the time in seconds from start of the day: ";
-	int seconds;
-	cin >> seconds;
-
-	int hours = (seconds/60)/60;
-	int minutes = (seconds - hours*60*60)/60;
-	int secondsNow = seconds - (seconds - hours * 60 * 60) - minutes*60;
-
-	cout << endl << "Time now is: " << hours << " hours " << minutes << " minutes " << seconds << " seconds ";
-
+	mistakes = 3;
+	displayHang(mistakes);
 
 	return 0;
 }
